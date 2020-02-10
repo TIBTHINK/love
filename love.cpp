fd6236991e7d04e73d0bafee0b174fed0c673f0b
tibthink <bent23jam@gmail.com>
2020-02-10 10:04:55 -0500

    just the simple thing at first since im at a terminal writing this

---
+++ love.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+    cout << "You are loved." << endl;
+    cout << "You might not feel like you are but there is some one out there for you." << endl;
+
+    return 0;
+}
