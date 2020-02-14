8dd3c0f2a5fa94b84939f6d91db9c972babee599
tibthink <bent23jam@gmail.com>
2020-02-14 01:24:02 +0000

    finnished the thingy

--- love.cpp
+++ love.cpp
@@ -1,24 +1,131 @@
 #include <iostream>
-#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
 
 using namespace std;
+void sleepcp(int milliseconds);
+#define clear() printf("\033[H\033[J")
 
-
-int main(){
-
-    
-    cout << "hey there friend" << endl;
-    usleep(10);
-
-    return 0;
+void sleepcp(int milliseconds) // Cross-platform sleep function
+{
+    clock_t time_end;
+    time_end = clock() + milliseconds * CLOCKS_PER_SEC/1000;
+    while (clock() < time_end)
+    {
+    }
 }
 
-    // std::
+void lizard()
+{
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::://+++ooooooooooooooooo++++////////::::::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::/+ooooooooooooooooooooooooooooooooooooo/::::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::/+ooooooooooooooooooooooooooooooooooooooooo/:::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::/yydNNdooooooooooooossssooooooooooooooooo+////+oo/::::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::sNMMMMNdooooooooooosmMMMMmsooooooooooooooo+////+ooo+:::::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::/+osyyyyssoooooooooooohMMMMMMhoooooooooooooooo++++oooooo/:::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::/++ooooooooooooooooooooooohmdhhysooooooooooooooooooooooooooo:::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::/+oooooooooooooooooooooooooooooooooooooooooo++/++ooooooooooooo+::::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::+ooooooooooooooooooooooooooooooooooooooooooo+/////+o+++ooooooooo+:::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::+ooooooooooooooooooooooooooooooooooooooooooooo+///+o/////+oooooooo:::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::+oooooooooooooooooooooooooooooooooooooooooooooooooooo//////oooooooo+::::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::oooooooooooooooooooooosoooooooooooooooooooo////+ooooo+///+oooooooooo/:::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::/+++oooooooooooooooooooooooooooooooooooooo//////ooooooooooooooooooooo:::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::///++oooooooooooooooooooooooooooooooo+/////ooooooooooooooooooooo+::::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::://////+++++++++++++++++++ooooooo+++ooooooooooooooooooooooo/:::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::+oooooooo++ooooooooo+++ooooooooo+:::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::/oooooo/////+ooooo+////+ooooooooo:::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::ooooo+//////oooooo////+ooooooooo:::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::/ooooo+///+oooooooo++ooooooooooo:::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::+oooooooooooooooooooooooooooooo+::::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::+oooooooooooooooooooooooooooooo+:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::+ooooooooo+////+ooooooooooooooo:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::oooooooo+//////ooo+////+oooooo:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::+oooooooo+////+ooo//////+ooooo:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::oooooooooooooooo+///////ooooo:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::ooooooooooooooooo//////+ooooo:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::/ooo++//+oooooooooo++++ooooooo:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::+oo+//////ooooooooooooooooooo/:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::ooo///////+oooooooooooooooooo/:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::ooo///////+oooooooooooooooooo/:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::/ooo///////ooooooooooooooooooo+:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::ooooo/////+oooooooooooooooooooo:::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::+ooooooo+ooooooooooooooooooooooo/::::::::::::::::" << endl;
+    sleepcp(100);
+    cout << "//////////////////////////////////////////////////+osssssssssssssssssssssssssssssss/////////////////" << endl;
+    sleepcp(100);
+    cout << "+++++++++++++++++++++++++++++++++++++++++++++++++oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo++++++++++++++++" << endl;
 
-    // cout << "hey there friend" << endl;
-    // std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-    // cout << "i know youve been having some troubles lately" << endl;
-    // std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    // cout << "be the person i know you can be" << endl;
-    // std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    // cout << "dont leave anything up to chance" << endl;
+}
+
+int main()
+{
+    clear();
+    sleepcp(14000);
+    cout << "hey there friend" << endl;
+    cout << "" << endl;
+    sleepcp(4000);
+    cout << "i know you've been having some troubles lately" << endl;
+    cout << "" << endl;
+    sleepcp(5000);
+    cout << "be the person i know you can be" << endl;
+    cout << "" << endl;
+    sleepcp(5000);
+    cout << "dont leave anything up to chance" << endl;
+    cout << "" << endl;
+    sleepcp(9000);
+    cout << "we ALL belive in you" << endl;
+    cout << "" << endl;
+    sleepcp(5000);
+    cout << "ur a winner kiddo." << endl;
+    cout << "dont you ever forget" << endl;
+    cout << "" << endl;
+
+    lizard();
+
+
+    // sleepcp(25000);
+
+    return 0;
+
+
+}
