9b37edb67ade976f56e773d696fdc1b63d2e28d9
tibthink <bent23jam@gmail.com>
2020-02-17 10:41:41 -0500

    added in termux support.. maybe

--- install.sh
+++ install.sh
@@ -1,10 +1,30 @@
-#!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
+
+if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
+    INSTALL_DIR="$PREFIX/usr/share/doc/fsociety"
+    BIN_DIR="$PREFIX/bin/"
+    BASH_PATH="$PREFIX/bin/bash"
+    TERMUX=true
+
+   apt install g++
+   ./comp.sh
+   mv love BIN_DIR
+   sleep 3
+   echo "ok now go away and run the love command :)"
+
+else
+    INSTALL_DIR="$HOME/.fsociety"
+    BIN_DIR="/usr/local/bin/"
+    BASH_PATH="/bin/bash"
+    TERMUX=false
+
+
+   if [[ $EUID -ne 0 ]]; then
+      echo "This script must be run as root" 
+      exit 1
+   fi
+   apt install g++
+   ./comp.sh
+   mv love /usr/bin/love
+   sleep 3
+   echo "ok now go away and run the love command :)"
 fi
-apt install g++
-./comp.sh
-mv love /usr/bin/love
-sleep 3
-echo "ok now go away and run the love command :)"
